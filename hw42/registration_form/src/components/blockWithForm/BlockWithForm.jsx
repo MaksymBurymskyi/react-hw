a407c836dad780032bd2729e72015635647e7167
Maksym84 <burimskiy84@gmail.com>
2023-03-20 00:56:15 +0200

    hw2-b

--- hw42/registration_form/src/components/blockWithForm/BlockWithForm.jsx
+++ hw42/registration_form/src/components/blockWithForm/BlockWithForm.jsx
@@ -1,27 +1,16 @@
 import React, { Component } from "react";
 import './BlockWithForm.css';
-import FormBlock from "../form_block/FormBlock";
+// import FormBlock from "../form_block/FormBlock";
+import FormikBlock from "../formik_block/FormikBlock";
 
 class BlockWithForm extends Component {
-  constructor(){
-    super();
-    this.state = {
-      checkNameResult: 'too short name',
-      checkEmailResult: '',
-      email: false,
-      checPasswordkResult: '',
-      styleNameBgd: 'transparent',
-      styleEmailBgd: 'transparent',
-      stylePasswordBgd: 'transparent',
-    }; 
-  }
   
   render() {
     return (
       <div className="formBlock">
         <h2 className="formBlock__title">Create an account</h2>
         <p className="formBlock__subTitle">Let’s get started with your 30 days free trial</p>
-        <FormBlock></FormBlock>
+        <FormikBlock></FormikBlock>
         <p className="formBlock__signInLink">Already have an account?<a href="/">Sign in</a></p>
       </div> 
       );
--- hw42/registration_form/src/components/formik_block/FormikBlock.jsx
+++ hw42/registration_form/src/components/formik_block/FormikBlock.jsx
@@ -1,130 +1,80 @@
 // import React, { Component } from "react";
-import './FormBlock.css';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import "./FormikBlock.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
-
-
-  
 const FormikBlock = () => (
   <Formik
-    initialValues={{ name: '', email: '' }}
-    validate={values => {
+    initialValues={{ name: "", email: "", password: "" }}
+    validate={(values) => {
       const errors = {};
-      if (!values.name) {
-        errors.name = 'Required';
+      
+      if(!values.name) {
+        errors.name = "Required";
+      } else if (values.name.length < 2) {
+        errors.name = "too short name";        
+      } else if (/\d+/g.test(values.name)) {
+        errors.name = 'only alphabets allowed';
       }
       if (!values.email) {
-        errors.email = 'Required';
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+      if (!values.password) {
+        errors.password = "Required";
+      } else if (values.password.length < 4) {
+        errors.password = "too short password";        
       }
       return errors;
     }}
     onSubmit={(values, { setSubmitting }) => {
       setTimeout(() => {
-        alert(JSON.stringify(values, null, 2));
+        console.log('data: ', JSON.stringify(values, null, 2));
         setSubmitting(false);
-      }, 400);
-    }}
-  >
+        alert("Дані успішно надіслано")
+      }, 800);
+    }}>
     {({ isSubmitting }) => (
       <Form className="formBlock__form">
-        <Field type="text" name="name" />
+        <label htmlFor="name">Name</label>
+        <Field
+          className="formBlock__inpField"
+          type="text"
+          name="name"
+          id="name"
+        />
         <ErrorMessage name="name" component="div" />
-        <Field type="email" name="email" />
+        <label htmlFor="email">Email</label>
+        <Field
+          className="formBlock__inpField"
+          type="email"
+          name="email"
+          id="email"
+        />
         <ErrorMessage name="email" component="div" />
-        <Field type="password" name="password" />
+        <label htmlFor="password">Password</label>
+        <Field
+          className="formBlock__inpField"
+          type="password"
+          name="password"
+          id="password"
+        />
         <ErrorMessage name="password" component="div" />
-        
-        <button className="formBlock__submitBtn" type="submit" disabled={isSubmitting}>Create Account</button>
+
+        <button
+          className="formBlock__submitBtn"
+          type="submit"
+          disabled={isSubmitting}>
+          Create Account
+        </button>
+        <button className="formBlock__signUpBtn" type="button">
+          Sign up with Google
+        </button>
       </Form>
     )}
   </Formik>
 );
 
 export default FormikBlock;
-
-// class FormBlock extends Component {
-//   constructor(){
-//     super();
-//     this.state = {
-//       checkNameResult: 'too short name',
-//       checkEmailResult: '',
-//       email: false,
-//       checPasswordkResult: '',
-//       styleNameBgd: 'transparent',
-//       styleEmailBgd: 'transparent',
-//       stylePasswordBgd: 'transparent',
-//     }; 
-//   }
-  
-//  checkName = (event) => {
-//     const name = event.target.value;
-//     const isNum = /[0-9]/.test(name);
-
-//     if (isNum) {
-//       event.target.value = name.slice(0, -1);
-//    }
-//    this.setState(_prevState => ({
-//      checkNameResult: name.length < 2 ? 'too short name' : '',
-//      styleNameBgd: name.length < 2 ? 'rgba(255, 0, 0, 0.8)' : 'transparent',
-//   }));  
-//   }
-
-  // checkEmail = (event) => {
-  //   const email = event.target.value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)
-
-  //   this.setState(() => ({
-  //     email: email ? email[0] : false,
-  //     checkEmailResult: email ? '' : 'wrong email',
-  //     styleEmailBgd: email ? 'transparent' : 'rgba(255, 0, 0, 0.8)',
-  //   }))
-  //   console.log(email);
-  // }
-
-  // handleSubmit = (event) => {
-  //   event.preventDefault()  
-
-  //   if (this.state.email && !this.state.checkNameResult && this.state.checPasswordkResult) {
-  //     event.target.form.submit()
-  //   } else {
-  //     return alert('Перевірте правильність вводу')
-  //   }
-  // }
-
-//   checkPassword = (event) => {
-//     if (event.target.value.length < 4) {
-//       this.setState({ checPasswordkResult: 'short password',
-//        stylePasswordBgd: 'rgba(255, 0, 0, 0.8)' });
-//     } else if (event.target.value.length < 8) {
-//       this.setState({ checPasswordkResult: 'average password',
-//        stylePasswordBgd: 'rgba(60, 190, 240, 0.8)' });
-//     }else  {
-//       this.setState({ checPasswordkResult: 'good password length',
-//        stylePasswordBgd: 'rgba(0, 200, 20, 0.9)' });
-//     }
-//   }
-  
-//   render() {
-//     return (
-//       <div className="formBlock">
-//         <h2 className="formBlock__title">Create an account</h2>
-//         <p className="formBlock__subTitle">Let’s get started with your 30 days free trial</p>
-//         <form className="formBlock__form" action="#">
-//           <label htmlFor="name">Name</label>
-//           <input type="text" name="name" id="name" onKeyUp={this.checkName} />
-//           <div className="formBlock__inpIndicator" style={{background: this.state.styleNameBgd}}>{this.state.checkNameResult }</div>
-//           <label htmlFor="email">Email</label>
-//           <input type="email" name="email" id="email" onChange={this.checkEmail}/>
-//           <div className="formBlock__inpIndicator" style={{background: this.state.styleEmailBgd}}>{this.state.checkEmailResult }</div>
-//           <label htmlFor="password">Password</label>
-//           <input type="password" name="password" id="password" onChange={this.checkPassword}/>
-//           <div className="formBlock__inpIndicator" style={{background: this.state.stylePasswordBgd}}>{this.state.checPasswordkResult }</div>
-//           <button className="formBlock__submitBtn" type="submit" onClick={this.handleSubmit}>Create Account</button>
-//           <button className="formBlock__signUpBtn" type="button">Sign up with Google</button>
-//         </form>
-//         <p className="formBlock__signInLink">Already have an account?<a href="/">Sign in</a></p>
-//       </div> 
-//       );
-//   }
-// }
-
-// export default FormBlock;
