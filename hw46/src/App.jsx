f8c6588cbcf17b82a862a33b9a2305b3d6e95f42
Maksym84 <burimskiy84@gmail.com>
2023-03-31 11:18:27 +0300

    middle save hw46

--- hw46/src/App.jsx
+++ hw46/src/App.jsx
@@ -1,23 +1,7 @@
 import './App.css';
 import WebFont from "webfontloader";
 import { useEffect } from "react";
-import Blog from './components/blog/Blog'
-import Header from './components/header/Header';
-import About from './components/about/About';
-import NotFound from './components/notFound/NotFound';
-import Article from './components/article/Article';
-import {
-  BrowserRouter, 
-  Routes, 
-  Route, 
-  // Switch,
-  // Link, 
-  // NavLink, 
-  // useParams,
-  // useNavigate
-} from 'react-router-dom';
-
-
+import ToDoList from './components/toDoList/ToDoList';
 
 
 function App() {
@@ -25,27 +9,14 @@ function App() {
   useEffect(() => {
     WebFont.load({
       google: {
-        families: ["Lora"],
+        families: ['Inter'],
       },
     });
   }, []);
     
-  function Contacts() {
-    return <div>Contacts</div>
-  }
-
   return (<>
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/blog' element={<Blog />} />
-        <Route path='*' element={<Blog />} />
-        <Route path='/About' element={<About />} />
-        <Route path='/contacts' element={<Contacts />} />
-        <Route path='/article/:id' element={<Article />} />
-        <Route path='/404' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ToDoList>
+    </ToDoList>
   </>)
 }
 
---
+++ hw46/src/components/toDoList/ToDoList.jsx
@@ -0,0 +1,7 @@
+import "./ToDoList.scss";
+
+export default function ToDoList() {
+  return <div className="toDoList">
+    <h2 className="toDoList__title">ToDo List</h2>
+  </div>
+}
