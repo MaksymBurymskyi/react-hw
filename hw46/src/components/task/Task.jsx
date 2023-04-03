26125e4b215d4b7e0222b99ec34b922f2453f1c5
Maksym84 <burimskiy84@gmail.com>
2023-04-03 15:50:49 +0300

    try 2

--- hw46/src/components/task/Task.jsx
+++ hw46/src/components/task/Task.jsx
@@ -1,28 +1,50 @@
 import './Task.scss';
-import { useForm,  } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import { useState } from 'react';
 
-export default function Task({ newTask }) {
+export default function Task({ newTask, onCheck, toRemove }) {
   const { handleSubmit } = useForm();
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
   
-  const toggleClass = () => {
-    document.querySelector('.task__text').classList.toggle('task__text-done');
-    document.querySelector('.task__checkbox').classList.toggle('task__checkbox-checked');
+  const onSubmit = (_data, event, formId) => {
+    event.preventDefault();
+
+    setIsButtonClicked(prevState => {
+    if (prevState) {
+      toRemove(formId);
+      console.log("formId :>> ", formId);
+      setIsButtonClicked(false)
+    } else {
+      onCheck();
+    }
+  });
+    // if (isButtonClicked) {
+    //   toRemove (formId)
+    //   console.log('formId :>> ', formId);
+    //   setIsButtonClicked(false)
+    // } else {
+    //   onCheck()
+    // };
   }
 
-  const removeForm = (form) => {
-  form.remove();
-  }
+  // const removeForm = (form) => {
+  //   // if (newTask.isChecked) form.remove();
+  //   console.log(form.id);
+  //   toRemove(form.id);
+  // }
   
   return <>
-    <form className="task" onClick={handleSubmit(toggleClass)}> 
-      
-      <input className="task__checkbox" type="checkbox" />
-      <span className="task__text">{newTask}</span>
-      
-      <button className="task__submitBtn" type="button" onClick={() => removeForm(document.querySelector('.task'))}></button>  
+    <form className="task" id={newTask.key} onClick={(event) => handleSubmit((data) => onSubmit(data, event, newTask.key))(event)}> 
+      <div className="task__wrap" >
+      <input className={`task__checkbox ${newTask.isChecked ? 'task__checkbox-checked' : ''}`} type="checkbox" />
+      <span className={  `task__text ${newTask.isChecked ? 'task__text-done' : ''}` }>{newTask.nameTask}</span>
+      </div>
+      <button className="task__submitBtn" type="submit" onClick={() => setIsButtonClicked(true)}></button>  
+      {/* <button className="task__submitBtn" type="submit" onClick={handleSubmit(onSubmit)}></button>   */}
+      {/* <button className="task__submitBtn" type="submit" onClick={toRemove}></button>   */}
     </form> 
   </>;
 }
 
-
+// onClick={() => removeForm(document.getElementById(newTask.key))}
 
--- hw46/src/components/toDoList/ToDoList.jsx
+++ hw46/src/components/toDoList/ToDoList.jsx
@@ -1,29 +1,43 @@
 import "./ToDoList.scss";
 import React from "react";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useForm } from "react-hook-form";
 import Task from "../task/Task";
 
 
 export default function ToDoList() {
 
-  const [taskList, setTaskList] = useState([{ nameTask: '' }]);
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const [newTask, setNewTask] = useState({ nameTask: '' });
+  const [taskList, setTaskList] = useState([{ nameTask: 'Почніть додавати задачі', isChecked: false , key: 0}]);
+  const { register, handleSubmit, reset, formState: { errors } } = useForm();
+  const [key, setKey] = useState(1)
   
-  const addTask = (task) => { 
-    setTaskList(prevTaskList => [...prevTaskList, newTask]);
-    setNewTask({ nameTask: task });
-  };
+  // useEffect(() => {
+  //   console.log('taskList :>> ', taskList);
+  // }, [taskList]);
 
-  const onSubmit = data => {
-    console.log(data.inputTask);
-    setNewTask({ nameTask: data.inputTask });
-    addTask(data.inputTask);
-    // data.inputTask = '0';
-    console.log(data.inputTask);
-  };
-  
+  const onSubmit = (data, event) => {
+    event.preventDefault();
+    setKey(prevkey => prevkey + 1)
+    setTaskList(prevTaskList => [{ nameTask: data.inputTask, isChecked: false, key: key }, ...prevTaskList]);
+    reset();
+  }
+
+  const toggleCheck = index => {
+    const forCheckList = [...taskList];
+    forCheckList[index].isChecked = !forCheckList[index].isChecked;
+    const chekcedItemsList = forCheckList.filter(item => item.isChecked === true)
+    const unChekcedItemsList = forCheckList.filter(item => item.isChecked === false)
+    const newList = unChekcedItemsList.concat(chekcedItemsList);
+    setTaskList(newList );
+  }
+
+  const remove = remId => {
+    const forCheckList = [...taskList];
+    const cleredList = forCheckList.filter(item => item.key !== remId);
+    setTaskList(cleredList );
+    console.log('remId :>> ', remId);
+  }
+    
   return <div className="toDoList">
     <h2 className="toDoList__title">ToDo List</h2>
     <form className="toDoList__form" onSubmit={handleSubmit(onSubmit)}>
@@ -35,7 +49,9 @@ export default function ToDoList() {
     </form>  
     
     <div className="toDoList__tasksWrapper">
-      {taskList.map((task, num) => <Task newTask={task.nameTask} key={ num} />)}
+      {taskList.map((task, num) => <Task newTask={task} key={num} id={num} onCheck={() => toggleCheck(num)} toRemove={ remove } />)}
     </div>
+
+    <p className="toDoList__quantity">Загальна кількість встановлених задач - {taskList.length} </p>
   </div>
 }
